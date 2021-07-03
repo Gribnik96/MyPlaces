d9baed518228e5ab056b79509e49f12f378a1477
Nikita <nikita.gribin@icloud.com>
2021-07-03 10:36:05 +0300

    newPlacessave

--- MyPlaces/NewPlaceTableController.swift
+++ MyPlaces/NewPlaceTableController.swift
@@ -8,24 +8,51 @@
 import UIKit
 
 class NewPlaceTableController: UITableViewController {
-
+    
+    var newPlace: Place?
+    var imageIsChanged = false
+    
+    @IBOutlet var saveButton: UIBarButtonItem!
+    
+    @IBOutlet var placeImage: UIImageView!
+    @IBOutlet var placeName: UITextField!
+    @IBOutlet var placeLocation: UITextField!
+    @IBOutlet var placeType: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+      
+        
         tableView.tableFooterView = UIView()
+        
+        saveButton.isEnabled = false
+        placeName.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            
+            let cameraIcon = #imageLiteral(resourceName: "camera")
+            let photoIcon = #imageLiteral(resourceName: "photo")
             
             let actionSheet = UIAlertController(title: nil,
                                          message: nil,
                                          preferredStyle: .actionSheet)
             let cameraAction = UIAlertAction(title: "Camera",
                                              style: .default) { _ in
-                self.chooseImagePicker(source: .camera)            }
+                self.chooseImagePicker(source: .camera)
+                
+            }
+            cameraAction.setValue(cameraIcon, forKey: "image")
+            
             let photoAction = UIAlertAction(title: "Photo", style: .default) { _ in self.chooseImagePicker(source: .photoLibrary)
                 
             }
+            photoAction.setValue(photoIcon, forKey: "image")
             let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
             
             actionSheet.addAction(cameraAction)
@@ -37,7 +64,10 @@ class NewPlaceTableController: UITableViewController {
             view.endEditing(true)
         }
     }
-
+    @IBAction func cancelAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
 
@@ -46,15 +76,56 @@ extension NewPlaceTableController: UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
+    
+    
+    @objc private func textFieldChanged() {
+      
+        if placeName.text?.isEmpty == false {
+            saveButton.isEnabled = true
+        } else {
+            saveButton.isEnabled = false
+        }
+    }
+    
+    func saveNewPlace() {
+        
+        var image: UIImage?
+        if imageIsChanged {
+            image = placeImage.image
+        } else {
+            image = #imageLiteral(resourceName: "imagePlaceholder")
+        }
+        
+        
+        newPlace = Place(name: placeName.text!, location: placeLocation.text, type: placeType.text, image: image, restaurantImage: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
 }
-extension NewPlaceTableController {
+extension NewPlaceTableController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     func chooseImagePicker (source: UIImagePickerController.SourceType) {
         if UIImagePickerController.isSourceTypeAvailable(source) {
             let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
             imagePicker.allowsEditing = true
             imagePicker.sourceType = source
             
             present(imagePicker, animated: true, completion: nil)
         }
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        placeImage.image = info[.editedImage] as? UIImage
+        placeImage.contentMode = .scaleAspectFill
+        placeImage.clipsToBounds = true
+        imageIsChanged = true
+        dismiss(animated: true, completion: nil)
     }
 }
--- MyPlaces/PlaceModel.swift
+++ MyPlaces/PlaceModel.swift
@@ -5,14 +5,15 @@
 //  Created by Nikita Gribin on 29.06.2021.
 //
 
-import Foundation
+import UIKit
 
 
 struct Place {
     var name: String
-    var location: String
-    var type: String
-    var image: String
+    var location: String?
+    var type: String?
+    var image: UIImage?
+    var restaurantImage: String?
     
     
  static   let restaurantNames = [
@@ -27,7 +28,7 @@ struct Place {
     var places = [Place]()
         
         for place in restaurantNames {
-            places.append(Place(name: place, location: "Moscow", type: "Restuarnt", image: place))
+            places.append(Place(name: place, location: "Moscow", type: "Restaunt", image: nil, restaurantImage: place))
         }
         
         return places
